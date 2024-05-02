e7562c4d2f1542985cb72d3fa505b6372de47123
maheep soni <maheepsoni6453@gmail.com>
2024-05-02 16:36:29 +0530

    commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// const fade =()=>{
+//     const wrapper = document.querySelector('.wrappper_container');
+//     wrappper_container.classList.add('fade');
+// };
+// window.addEventListener('load',fade);
